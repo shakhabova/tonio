f7a98c507b7cf4bed0892002b2e8bc1ecd53ea55
Алина Шахабова <alina.shakhabova@yandex.ru>
2024-05-23 17:26:08 +0300

    send money tabs

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,12 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+type Tabs = 'send-money' | 'receive-money';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+  ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
+  public currentTab: Tabs = 'send-money';
 
+  public get isSendMoneyTabActive(): boolean {
+    return this.currentTab === 'send-money';
+  }
+
+  public get isReceiveMoneyTabActive(): boolean {
+    return this.currentTab === 'receive-money';
+  }
+
+  public onSendMoneyTab(): void {
+    this.currentTab = 'send-money';
+  }
+
+  public onReceiveMoneyTab(): void {
+    this.currentTab = 'receive-money';
+  }
 }
