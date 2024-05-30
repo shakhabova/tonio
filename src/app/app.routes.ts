16e7e086ff0fe72752195e321ec2d6ac86d2c104
Алина Шахабова <alina.shakhabova@yandex.ru>
2024-05-30 12:57:59 +0300

    contact us component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { TrackATransferComponent } from './components/track-a-transfer/track-a-transfer.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
 
 export const routes: Routes = [
   
   { path: '', component:HomePageComponent , title: 'Tonio' },
-  { path: 'track-transfer', component:TrackATransferComponent, title: 'Track a transfer'}
+  { path: 'track-transfer', component:TrackATransferComponent, title: 'Track a transfer'},
+  { path: 'contact-us', component: ContactUsComponent, title: 'Contact Us'}
 ];
