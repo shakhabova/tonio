67d939991df23b3fb4c9e5338389a7b6d75ed897
Алина Шахабова <alina.shakhabova@yandex.ru>
2024-06-27 14:36:26 +0300

    business page ready

--- src/app/components/go-to-business/collaborate/collaborate.component.ts
+++ src/app/components/go-to-business/collaborate/collaborate.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+interface CollaborateCardModel {
+  title: string;
+  subtitle: string;
+  imgSrc: string;
+}
 
 @Component({
   selector: 'app-collaborate',
@@ -8,5 +14,45 @@ import { Component } from '@angular/core';
   styleUrl: './collaborate.component.css'
 })
 export class CollaborateComponent {
+  cards: CollaborateCardModel[] = [
+    {
+      title: 'Global coverage',
+      subtitle: 'Access to international (SEPA, SWIFT) and domestic payment systems',
+      imgSrc: 'assets/images/benefir.webp',
+    },
+    {
+      title: 'Flexible infrastructure',
+      subtitle: 'Cutting-edge technology for smooth and hassle-free transactions',
+      imgSrc: 'assets/images/Benefits.webp',
+    },
+    {
+      title: 'Beneficial fees',
+      subtitle: 'Generate revenue on a commissions earned from every transaction and increased business traffic',
+      imgSrc: 'assets/images/Loyalty.webp',
+    },
+    {
+      title: 'Hassle-free onboarding',
+      subtitle: 'A dedicated team to navigate and facilitate the onboarding process for you',
+      imgSrc: 'assets/images/structure.webp',
+    },
+    {
+      title: 'Agent loyalty program',
+      subtitle: 'Get additional benefits from your referal to other agents',
+      imgSrc: 'assets/images/Onboarding.webp',
+    },
+    {
+      title: 'Secure transfers',
+      subtitle: 'Fully compliant infrastructure for secure and frictionless transactions',
+      imgSrc: 'assets/images/secure.webp',
+    },
+  ]
+  currentCard = signal(this.cards[0]);
 
+  selectCard(card: CollaborateCardModel): void {
+    if (this.currentCard().title === card.title) {
+      return;
+    }
+
+    this.currentCard.set(card);
+  }
 }
--- src/app/components/go-to-business/contact-form/contact-form.component.ts
+++ src/app/components/go-to-business/contact-form/contact-form.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { COUNTRIES } from './constants';
+import { CommonModule } from '@angular/common';
+import { NgSelectComponent, NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    NgSelectModule,
+  ],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.css'
 })
 export class ContactFormComponent {
-
+  countries = COUNTRIES;
+  currentCountry = this.countries[0];
 }
