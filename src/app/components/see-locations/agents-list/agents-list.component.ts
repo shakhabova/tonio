c9f0f1b9063de3311da4ca2d823a9e437a286f97
Алина Шахабова <alina.shakhabova@yandex.ru>
2024-08-01 09:28:45 +0300

    pagination added

--- src/app/components/see-locations/agents-list/agents-list.component.ts
+++ src/app/components/see-locations/agents-list/agents-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { PaginationComponent } from "../pagination/pagination.component";
 
 interface AgentModel {
   name: string;
@@ -11,48 +12,60 @@ interface AgentModel {
 @Component({
   selector: 'app-agents-list',
   standalone: true,
-  imports: [],
+  imports: [PaginationComponent],
   templateUrl: './agents-list.component.html',
   styleUrl: './agents-list.component.css'
 })
 export class AgentsListComponent {
   agents: AgentModel[] = [
     {
-      name: 'Agent Name',
+      name: 'Agent Name1',
       address: 'Clarendon ave. 78',
       phone: '+1 755 1002 678',
       email: 'h.kojima@gmail.com',
       workHours: 'Mon-Fri, 10 AM - 5 PM'
     },
     {
-      name: 'Agent Name',
+      name: 'Agent Name12',
       address: 'Clarendon ave. 78',
       phone: '+1 755 1002 678',
       email: 'h.kojima@gmail.com',
       workHours: 'Mon-Fri, 10 AM - 5 PM'
     },
     {
-      name: 'Agent Name',
+      name: 'Agent Name13',
       address: 'Clarendon ave. 78',
       phone: '+1 755 1002 678',
       email: 'h.kojima@gmail.com',
       workHours: 'Mon-Fri, 10 AM - 5 PM'
     },
     {
-      name: 'Agent Name',
+      name: 'Agent Name4',
       address: 'Clarendon ave. 78',
       phone: '+1 755 1002 678',
       email: 'h.kojima@gmail.com',
       workHours: 'Mon-Fri, 10 AM - 5 PM'
     },
-  ]
+  ];
 
-  filteredAgents = signal([...this.agents]);
+  displayAgents = computed(() => {
+    const filtered = this.filteredAgents();
+    const sliceStart = this.currentPageIndex() * this.pageSize;
+    const sliced = filtered.slice(sliceStart, sliceStart + this.pageSize);
+    return sliced;
+  });
+
+  filteredAgents = computed(() => this.agents.filter(agent => agent.name.toLowerCase().includes(this.search().toLowerCase())));
+
+  search = signal('');
+  currentPage = signal(1);
+  currentPageIndex = computed(() => this.currentPage() - 1);
+
+  readonly pageSize = 12;
 
   onSearch(event: Event): void {
     const inputEl: HTMLInputElement | null = event.target as HTMLInputElement;
-
     const searchText = inputEl.value;
-    this.filteredAgents.set(this.agents.filter(agent => agent.name.toLowerCase().includes(searchText.toLowerCase())));
+    this.search.set(searchText);
   }
 }
--- src/app/components/see-locations/pagination/pagination.component.ts
+++ src/app/components/see-locations/pagination/pagination.component.ts
@@ -1,57 +1,39 @@
-import { Component, Input, OnInit, SimpleChange } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, input, model, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pagination.component.html',
   styleUrl: './pagination.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PaginationComponent implements OnInit {
-  @Input()
-  collectionSize = 0;
+export class PaginationComponent implements OnChanges {
+  collectionSize = input(0);
+  pageSize = input(5);
+  currentPage = model(1);
+  maxSize = input(2);
+  firstLastButtons = input(false);
+  nextPreviousButtons = input(true);
+  small = input(false);
+  totalPages: any [] = [];
 
-  @Input()
-  pageSize = 5;
-
-  @Input()
-  currentPage = 1;
-
-  @Input()
-  maxSize = 2;
-
-  @Input ()
-  firstLastButtons = false;
-
-  @Input ()
-nextPreviusButtons = true;
-
-@Input () 
-small = false;
-
-totalPages: any [] = [];
-
-constructor() {}
-
-  ngOnInit(): void {
-    this.totalPages = new Array(Math.ceil(this.collectionSize / this.pageSize));
-  }
-
-  ngOnChanges(changes: SimpleChange) {
-    this.totalPages = new Array(Math.ceil(this.collectionSize / this.pageSize));
+  ngOnChanges() {
+    this.totalPages = new Array(Math.ceil(this.collectionSize() / this.pageSize()));
   }
 
   selectPageNumber(pageNumber: number) {
-    this.currentPage = pageNumber;
+    this.currentPage.set(pageNumber);
   }
 
   next() {
-    const nextPage = this.currentPage + 1;
+    const nextPage = this.currentPage() + 1;
     nextPage <= this.totalPages.length && this.selectPageNumber(nextPage);
   }
 
   previous() {
-    const previousPage = this.currentPage - 1;
-    previousPage >= && this.selectPageNumber(previousPage);
+    const previousPage = this.currentPage() - 1;
+    previousPage >= 0 && this.selectPageNumber(previousPage);
   }
 }
